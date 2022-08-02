b61862da4aedbcfde12cebb4312659dc1178a06a
Fernando J Monteiro <ferjmonteiro@gmail.com>
2022-08-02 14:48:21 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+it('se a página contém um heading h2 com o texto Page requested not found', () => {
+  const { history } = renderWithRouter(<App />);
+  history.push('/coringa');
+  const pageNotFound = screen.getByRole('heading', { name: /page requested not found/i });
+  expect(pageNotFound).toBeInTheDocument();
+});
+
+it('se a página mostra a imagem', () => {
+  const { history } = renderWithRouter(<App />);
+  history.push('/qualquerlugar');
+  const imageUrl = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+  const img = screen
+    .getByAltText(/Pikachu crying because the page requested was not found/i);
+  expect(img.src).toContain(imageUrl);
+});
